3c2163144d69b98322ad85dbc68a89a0fe0bf543
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-11-07 10:26:59 +0530

    Adding program to check maximum number without generics

--- src/com/bridgelabz/maximum_with_generics/MaximumTest.java
+++ src/com/bridgelabz/maximum_with_generics/MaximumTest.java
@@ -2,6 +2,21 @@ package com.bridgelabz.maximum_with_generics;
 
 public class MaximumTest {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Genrics Program");
+		//Checking maximum with integers
+		computeMax(100, 20, 30);
+		computeMax(10, 100, 30);
+		computeMax(40, 20, 100);
 	}
+
+	private static void computeMax(Integer x, Integer y, Integer z) {
+		int max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+		System.out.println("Maximum Number is:" + max);
+	}
+
 }
