7dcd922ce2a79cec1716b01c9de5d78822f19849
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-11-07 12:39:44 +0530

    Adding function with the input of more than 3 parameters

--- src/com/bridgelabz/maximum_with_generics/MaximumTest.java
+++ src/com/bridgelabz/maximum_with_generics/MaximumTest.java
@@ -4,27 +4,27 @@ public class MaximumTest {
 	
 	public static void main(String[] args) {
 		// Checking maximum with integers
-		computeMax(100, 20, 30);
-		computeMax(10, 100, 30);
-		computeMax(40, 20, 100);
+		testMaximum(100, 20, 30,40,50,50,101);
+		testMaximum(10, 100, 30);
+		testMaximum(40, 20, 100);
 		// Checking with the 3 float values
-		computeMax(100.0F, 20.0F, 30.0F);
-		computeMax(10.0F, 500.1F, 30.2F);
-		computeMax(40.4F, 20.7F, 300.9F);
-		computeMax("Apple","Peach","Banana");
-		computeMax("Peach","Apple","Banana");
-		computeMax("Peach","Banana", "Apple");
+		testMaximum(100.0F, 20.0F, 30.0F);
+		testMaximum(10.0F, 500.1F, 30.2F);
+		testMaximum(40.4F, 20.7F, 300.9F);
+		testMaximum("Apple","Peach","Banana");
+		testMaximum("Peach","Apple","Banana");
+		testMaximum("Peach","Banana", "Apple");
 	}
 
 
-	private static <T extends Comparable<T>> void computeMax(T x, T y, T z) {
-		T max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0) {
-			max = z;
-		}
+	@SafeVarargs
+	private static <T extends Comparable<T>> void testMaximum(T...input) {
+		T max = input[0];
+		for(int i=0; i<input.length; i++) {
+			if(input[i].compareTo(max)>0) {
+				max=input[i];
+			}
+		}	
 		System.out.println("Maximum Number is:" + max);
 	}
 
