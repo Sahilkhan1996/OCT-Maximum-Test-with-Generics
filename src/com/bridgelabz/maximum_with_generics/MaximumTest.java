1de768ced83563993c871c8c2ed0fb2cad4ed95e
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-11-07 12:47:12 +0530

    calling print method internally

--- src/com/bridgelabz/maximum_with_generics/MaximumTest.java
+++ src/com/bridgelabz/maximum_with_generics/MaximumTest.java
@@ -1,30 +1,33 @@
 package com.bridgelabz.maximum_with_generics;
 
 public class MaximumTest {
-	
+
 	public static void main(String[] args) {
 		// Checking maximum with integers
-		testMaximum(100, 20, 30,40,50,50,101);
+		testMaximum(100, 20, 30, 40, 50, 50, 101);
 		testMaximum(10, 100, 30);
 		testMaximum(40, 20, 100);
 		// Checking with the 3 float values
-		testMaximum(100.0F, 20.0F, 30.0F);
+		testMaximum(100.0F, 20.0F, 30.0F, 100F, 200F);
 		testMaximum(10.0F, 500.1F, 30.2F);
 		testMaximum(40.4F, 20.7F, 300.9F);
-		testMaximum("Apple","Peach","Banana");
-		testMaximum("Peach","Apple","Banana");
-		testMaximum("Peach","Banana", "Apple");
+		testMaximum("Apple", "Peach", "Banana", "PineApple");
+		testMaximum("Peach", "Apple", "Banana");
+		testMaximum("Peach", "Banana", "Apple");
 	}
 
-
 	@SafeVarargs
-	private static <T extends Comparable<T>> void testMaximum(T...input) {
+	private static <T extends Comparable<T>> void testMaximum(T... input) {
 		T max = input[0];
-		for(int i=0; i<input.length; i++) {
-			if(input[i].compareTo(max)>0) {
-				max=input[i];
+		for (int i = 0; i < input.length; i++) {
+			if (input[i].compareTo(max) > 0) {
+				max = input[i];
 			}
-		}	
+		}
+		printMax(max);
+	}
+
+	private static <T> void printMax(T max) {
 		System.out.println("Maximum Number is:" + max);
 	}
 
